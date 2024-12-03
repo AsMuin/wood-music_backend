d0020bfb9eccb7ba56ebc00fae7c31ad97d2d36c
AsMuin <asmuins@foxmail.com>
2024-12-03 03:21:42 +0000

    perf: 错误捕获

--- src/utils/pageQuery.ts
+++ src/utils/pageQuery.ts
@@ -6,16 +6,21 @@ interface IPageQueryResult<T>{
     isEnd:boolean
 }
 const pageQuery = <T=any>(Model:mongoose.Model<T>)=>async (pageIndex:number,pageSize:number) =>{
-    const itemListPromise = Model.find().skip(pageIndex*pageSize).limit(pageSize).exec()
-    const totalPromise = Model.countDocuments().exec()
-    const [itemList,total] = await Promise.all([itemListPromise,totalPromise])
-    const isEnd = (pageIndex+1)*pageSize>=total
-    const result:IPageQueryResult<T>={
-        itemList,
-        total,
-        isEnd
+    try{
+        const itemListPromise = Model.find().skip(pageIndex*pageSize).limit(pageSize).exec()
+        const totalPromise = Model.countDocuments().exec()
+        const [itemList,total] = await Promise.all([itemListPromise,totalPromise])
+        const isEnd = (pageIndex+1)*pageSize>=total
+        const result:IPageQueryResult<T>={
+            itemList,
+            total,
+            isEnd
+        }
+        return result
+    }catch(err:any){
+        console.error(err)
+       return Promise.reject(err)
     }
-return result
 }
 
 export default pageQuery
